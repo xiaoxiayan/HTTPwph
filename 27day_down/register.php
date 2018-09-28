1b3c1a782bcf94da6d8f7b9b9142bd08ab198371
LLLQW <a690150618@qq.com>
2018-09-28 21:06:53 +0800

    未完成注册验证功能，GET找不到

---
+++ 27day_down/register.php
@@ -0,0 +1,15 @@
+<?php
+	$username = $_GET['username'];
+	var_dump($username);
+	echo "1";
+//	$coon = new mysqli("localhost", "root", "", "shophao", "3306");
+//	$sql = "select * from shop_user where username = '$username'";
+//  $result = $coon -> query($sql);
+//  $rows = $result -> fetch_assoc();
+//  if($rows) {
+//      // 账号已存在
+//      $arr = array("msg" => 101);
+//  } else {
+//      $arr = array("msg" => 200);
+//  }
+?>
